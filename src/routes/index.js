14b4acd3b5bbdece0716241cb055b0e7b55c25eb
mbmugdho <mdmontasirbillah5@gmail.com>
2026-01-08 00:25:31 +0600

    mount task and payment routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
 import statsRoutes from "./statsRoutes.js";
+import taskRoutes from "./taskRoutes.js";
+import paymentRoutes from "./paymentRoutes.js";
 
 const router = Router();
 
@@ -14,5 +16,8 @@ router.use("/users", userRoutes);
 
 // Phase 6 (public)
 router.use("/stats", statsRoutes);
+
+router.use("/tasks", taskRoutes);
+router.use("/payments", paymentRoutes);
 
 export default router;
